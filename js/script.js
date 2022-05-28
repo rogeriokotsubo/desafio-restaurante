405cc07be59977ac70471c8d9bfad0dd2d587a66
Rogerio Kotsubo <rogeriokotsubo@gmail.com>
2022-05-27 21:07:34 -0300

    chore: backing up

---
+++ js/script.js
@@ -0,0 +1,11 @@
+//Creditos: CodingLab
+
+let navLinks = document.querySelector(".nav-links");
+let menuOpenBtn = document.querySelector(".navbar .bx-menu");
+let menuCloseBtn = document.querySelector(".nav-links .bx-x");
+menuOpenBtn.onclick = function() {
+navLinks.style.left = "0";
+}
+menuCloseBtn.onclick = function() {
+navLinks.style.left = "-100%";
+}
