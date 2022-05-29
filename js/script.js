175d65209af6c1839d3169c2da5943bdd0812712
Rogerio Kotsubo <rogeriokotsubo@gmail.com>
2022-05-29 00:28:50 -0300

    chore: v1

--- js/script.js
+++ js/script.js
@@ -9,3 +9,15 @@ navLinks.style.left = "0";
 menuCloseBtn.onclick = function() {
 navLinks.style.left = "-100%";
 }
+
+//Creditos: CodingLab
+
+let lojaArrow = document.querySelector(".sub-menu");
+lojaArrow.onclick = function() {
+ navLinks.classList.toggle("show1");
+}
+
+let atendimentoArrow = document.querySelector(".sub-menu");
+atendimentoArrow.onclick = function() {
+ navLinks.classList.toggle("show3");
+}
